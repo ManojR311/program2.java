5603dde333bf1a8372265232eb72b69b9f9c7e8b
ManojR311 <71012451+ManojR311@users.noreply.github.com>
2021-08-27 08:37:51 +0530

    Add files via upload

---
+++ program2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+
+int num=10; int val=1;
+System.out.println("input a = "+n+" then output: ");
+Scanner input = new Scanner(System.in);
+num = input.nextInt();
+if(num==1){
+    System.out.println(1);
+}
+else{
+    System.out.println(1);
+    for(int i=1;i<num;){
+        val=val+2;
+        i=i+1;
+        System.out.println(val);
+    }
+}
+}
+}
